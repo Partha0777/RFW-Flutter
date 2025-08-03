3c0a6aed79f43c7847b00f5ce762145fad543e83
Partha0777 <112649441+Partha0777@users.noreply.github.com>
2025-08-03 21:41:42 +0530

    Integrated the remote widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:rfw/formats.dart';
 import 'package:rfw/rfw.dart';
 
 void main() {
